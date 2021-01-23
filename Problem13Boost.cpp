53d071702c614cf8a7071ee95adac2cbaa93d516
Marcus Ong <ong1.marcusb@gmail.com>
2021-01-23 15:46:17 +0800

    Problem 13 using Boost Libraries for big numbers

---
+++ Problem13Boost.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <fstream>
+using namespace std;
+
+// Linux CLI to get boost header files
+// Command: $ sudo apt-get install libboost-all-dev
+// Installs header files to be readily included, reduces hassle :)
+
+int main()
+{
+    boost::multiprecision::cpp_int sum = 0, i;
+    ifstream numFile("Problem13.txt");
+
+    if (numFile.is_open())
+    {
+        while (numFile >> i)
+        {
+            sum+=i;
+        }
+    }
+    numFile.close();
+    cout << sum << endl;
+}
